a995de00ff8f221acf791b59ef676e64e29f6191
zgy <1349604075@qq.com>
2026-06-14 21:37:07 +0800

    Initial Commit

---
+++ one/oneApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneApp.swift
+//  one
+//
+//  Created by zgy on 6/14/26.
+//
+
+import SwiftUI
+
+@main
+struct oneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
